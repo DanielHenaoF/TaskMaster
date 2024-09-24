02f647e4ae22168a4836de6e37ec249b6f62fc67
DanielHenaoF <Daniielfh@outlook.com>
2024-09-24 16:19:20 -0500

    Finalización del Back

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 
 // Usando DB_URI para la conexión
 const sequelize = new Sequelize(process.env.DB_URI, {
-  dialect: "mysql",
+  dialect: "postgresql",
   logging: console.log,
 });
 
