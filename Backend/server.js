29074825de1a608b5da10ca6752cb1c142573fb9
DanielHenaoF <Daniielfh@outlook.com>
2024-09-25 22:02:27 -0500

    Finalización del Back

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Configurar CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://taskmaster-flax-nu.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
